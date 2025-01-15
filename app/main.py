7d2267dc3fa260fc7f034b1abf9b6825d56e5649
Rodrigo Coura Torres <torres.rc@gmail.com>
2025-01-15 12:24:34 -0300

    a

--- app/main.py
+++ app/main.py
@@ -5,8 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def read_env():
-    # Get all environment variables and format them as JSON
-    env_vars = {}
-    for key, value in os.environ.items():
-        env_vars[key] = value
-    return env_vars
+    return {'env': os.environ['ENVIRONMENT']}
